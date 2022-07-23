c58863f264aae6ec2ca684aa0a461b41028844f1
alexander-kulyk <kulyk.alexander@gmail.com>
2022-07-23 15:59:04 +0300

    masonry js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // import Masonry from 'masonry-layout';
 
-// window.onload = () =>{
-//     const grid = document.querySelector('.grid');
+window.onload = () =>{
+    const grid = document.querySelector('.grid');
 
-//     const masonry = new Masonry(grid);
-// };
+    const masonry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        gutter: 10,
+        originLeft: false,
+        originTop: false,
+    });
+
+    masonry.on('layoutComplete', () => console.log('layout Complete'));
+
+};
+
