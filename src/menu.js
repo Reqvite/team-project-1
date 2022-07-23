f400fa7da81195528bb437552442d3590be65dfb
VladyslavLysenko <Dodog12362@gmail.com>
2022-07-23 15:36:55 +0300

    ok

---
+++ src/menu.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-open__btn'),
+    closeMenuBtn: document.querySelector('.close-menu__btn'),
+    menu: document.querySelector('.mob__menu'),
+    body: document.querySelector('body'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+
