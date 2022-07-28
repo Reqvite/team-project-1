f0975e891e18ebf1285549bb52c1b3e4f6799d51
Reqvite <reqvite2233@gmail.com>
2022-07-28 13:16:08 +0300

    final

--- src/js/menu.js
+++ src/js/menu.js
@@ -7,12 +7,14 @@
     closeMenuLink: document.querySelector('[data-nav-link]'),
     closeMenuLinkContact: document.querySelector('[data-nav-linkC]'),
     closeMenuLinkProduct: document.querySelector('[data-nav-linkP]'),
+    closeMenuLinkAbout: document.querySelector('[data-nav-linkA]'),
   };
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuLink.addEventListener('click', toggleMenu);
   refs.closeMenuLinkContact.addEventListener('click', toggleMenu);
   refs.closeMenuLinkProduct.addEventListener('click', toggleMenu);
+  refs.closeMenuLinkAbout.addEventListener('click', toggleMenu);
   function toggleMenu() {
 		refs.menu.classList.toggle('is-none');
 		refs.menu.classList.toggle('menu-scroll');
